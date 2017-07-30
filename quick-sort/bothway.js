86f71fa2a9cd18e2314298a9b4f8b87ef735177e
lipeishang <1315789454@qq.com>
2017-07-30 20:24:46 +0800

    quick-sort-bothway

--- quick-sort/bothway.js
+++ quick-sort/bothway.js
@@ -1,3 +1,26 @@
 /**
  * Created by lipeishang on 17-7-30.
  */
+var arr = [1,4,5,2,7];
+function quickSort(list){
+    if(list.length == 0){
+        return [];
+    }
+    else{
+        var lesser = []; //小于基准值的序列
+        var greater = []; //大于基准值的序列
+        var pivot  = list[0];
+        for(var i=1; i<list.length; i++){
+            if (list[i] <= pivot){
+                lesser.push(list[i]);
+            }
+            else{
+                greater.push(list[i]);
+            }
+        }
+    }
+    return quickSort(lesser).concat(pivot, quickSort(greater));
+}
+
+console.log(quickSort(arr));
+
