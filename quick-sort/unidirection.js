ccb6e2748c93ecf973c35325c221d7d6f3906ed7
lipeishang <1315789454@qq.com>
2017-07-30 20:02:17 +0800

    quick-sort-unidirection

---
+++ quick-sort/unidirection.js
@@ -0,0 +1,36 @@
+/**
+ * Created by lipeishang on 17-7-30.
+ */
+var arr = [1 ,7,2,5,4,9];
+function quicksort (arr) {
+    function swap (arr, m, n) {
+        var tmp = arr[m];
+        arr[m] = arr[n];
+        arr[n] = tmp;
+    }
+    function partition (arr, low, high) {
+        var x = arr[high];
+        var i = low - 1;
+        for(var j = low; j < high; j++) {
+            if(arr[j] <= x){
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+    function sort (arr, low, high) {
+        if(low < high) {
+            var pivotIndex = partition(arr, low, high);
+            sort(arr, low, pivotIndex - 1);
+            sort(arr, pivotIndex + 1, high);
+        }
+        return arr;
+    }
+
+    return sort(arr, 0, arr.length - 1);
+}
+
+console.log(quicksort(arr));
+
